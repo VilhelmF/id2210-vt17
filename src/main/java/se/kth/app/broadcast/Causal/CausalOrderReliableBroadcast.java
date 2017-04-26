8e69e326c21dd13db68f5c211821ac06237e43da
Sindri Már Kaldal Sigurjónsson <sindrikaldal@n147-p133.kthopen.kth.se>
2017-04-26 16:21:51 +0200

    subscribing handlers to ports

--- src/main/java/se/kth/app/broadcast/Causal/CausalOrderReliableBroadcast.java
+++ src/main/java/se/kth/app/broadcast/Causal/CausalOrderReliableBroadcast.java
@@ -77,5 +77,10 @@ public class CausalOrderReliableBroadcast extends ComponentDefinition {
         }
     }
 
+    {
+        subscribe(crbBroadcastHandler, crb);
+        subscribe(rbDeliverHandler, rb);
+    }
+
 
 }

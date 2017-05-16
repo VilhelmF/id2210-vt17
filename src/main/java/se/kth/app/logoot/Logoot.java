b49493ea918429018fe7eae352f720c9be6cbc95
Sindri Már Kaldal Sigurjónsson <sindrikaldal@n154-p62.kthopen.kth.se>
2017-05-16 18:29:02 +0200

    comparing line identifiers and positons with each other

--- src/main/java/se/kth/app/logoot/Logoot.java
+++ src/main/java/se/kth/app/logoot/Logoot.java
@@ -19,7 +19,7 @@ public class Logoot extends ComponentDefinition {
 
     protected HashMap<Integer, Patch> historyBuffer = new HashMap<>();
     protected List<LineIdentifier> identifierTable = new ArrayList<>();
-    protected HashMap<Integer, Integer> cemetary = new HashMap<>();
+    protected HashMap<LineIdentifier, Integer> cemetery = new HashMap<>();
     protected final Positive<Network> net = requires(Network.class);
 
 
@@ -84,7 +84,10 @@ public class Logoot extends ComponentDefinition {
     public void execute(Patch patch) {
         for (Operation op : patch.getOperations()) {
             if (op.getType().equals(OperationType.INSERT)) {
+                int degree = cemetery.get(op.getId());
+                if (degree == 1) {
 
+                }
             } else {
 
             }
@@ -155,6 +158,25 @@ public class Logoot extends ComponentDefinition {
             }
         }
         return inversePatch;
+    }
+
+    public int positionBinarySearch(LineIdentifier identifier) {
+
+        int low = 0;
+        int high = identifierTable.size() - 1;
+        
+        while(low <= high ) {
+            int middle = (low + high) / 2;
+            if (identifier.compareTo(identifierTable.get(middle)) == 1){
+                low = middle + 1;
+            } else if (identifierTable.get(middle).compareTo(identifier) == 1) {
+                high = middle - 1;
+            } else { // The element has been found
+                return middle;
+            }
+        }
+
+        return 0;
     }
 
     {
